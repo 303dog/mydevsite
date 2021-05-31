3aa1a23fbc2f29f7229185a2b8b166ce593fc278
Paul_Beckwith <303dog@gmail.com>
2021-05-31 17:37:12 -0600

    linked gitconnect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     // NOTE: Use your username below
-    fetch('https://gitconnected.com/v1/mydevsite/303dog')
+    fetch('https://gitconnected.com/v1/portfolio/303dog')
       .then(res => res.json())
       .then(user => {
         setUser(user);
